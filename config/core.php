425f6088c1dfe8d780e440ef1ff88006b0d651a3
Martin Ueding <dev@martin-ueding.de>
2011-08-12 13:34:59 +0200

    added new sec salts

--- config/core.php
+++ config/core.php
@@ -201,12 +201,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', '760aff1b-f06c-411a-9181-6c0de7d7255a');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '5675160984727693993356');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
