d665cc79c9bdfd2a5aacd7223cdb389c12b18bf4
Martin Ueding <dev@martin-ueding.de>
2011-08-15 21:46:28 +0200

    re-enabled redirect

--- controllers/bookmarks_controller.php
+++ controllers/bookmarks_controller.php
@@ -69,7 +69,7 @@ class BookmarksController extends AppController {
 		if (!empty($this->data)) {
 			if ($this->Bookmark->save($this->data) && $this->Keyword->save($this->data)) {
 				$this->Session->setFlash(__('The bookmark has been saved', true));
-				//$this->redirect(array('action' => 'index'));
+				$this->redirect(array('action' => 'index'));
 			} else {
 				$this->Session->setFlash(__('The bookmark could not be saved. Please, try again.', true));
 			}
