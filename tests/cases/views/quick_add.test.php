d86298bb1ff597375b1a0debcef803f5f3134cf3
Martin Ueding <dev@martin-ueding.de>
2011-08-21 21:28:04 +0200

    fixed test

--- tests/cases/views/quick_add.test.php
+++ tests/cases/views/quick_add.test.php
@@ -3,19 +3,27 @@
 
 class QuickAddTestCase extends CakeWebTestCase {
 	function __construct() {
-		$this->baseurl = "127.0.0.1".current(split("webroot", $_SERVER['PHP_SELF']));
+		$this->baseurl = "127.0.0.1".current(split("app", $_SERVER['PHP_SELF']));
 	}
 
 	function test_input_to_reading_list() {
 		$input = uniqid();
 
 		$this->get($this->baseurl."/");
+		$this->assertResponse(200);
+		$this->assertNoPattern("/$input/");
+		$this->assertNoPattern("/Missing Controller/");
+
 		$this->setField('data[Bookmark][title]', $input);
 		$this->setField('data[Bookmark][url]', "$input.tld");
 		$this->setField('data[Bookmark][reading_list]', "1");
 		$this->click("Create Bookmark");
 
 		$this->get($this->baseurl."/");
+		$this->assertResponse(200);
+		$this->assertNoPattern("/Missing Controller/");
+
+		$this->assertPattern("/newest/");
 		$this->assertPattern("/$input/");
 	}
 }
