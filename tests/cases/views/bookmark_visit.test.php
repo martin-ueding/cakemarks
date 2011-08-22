3af5eb00f699163e1024625cc8adee0e6e39746b
Martin Ueding <dev@martin-ueding.de>
2011-08-22 18:52:08 +0200

    removed test for startscreen

--- tests/cases/views/bookmark_visit.test.php
+++ tests/cases/views/bookmark_visit.test.php
@@ -35,33 +35,5 @@ class BookmarkVisitTestCase extends CakemarksWebTestCase {
 		$this->assertPattern("/Visits: 1\D+/");
 		$this->assertNoPattern("/Visits: [02-9]\d*/");
 	}
-
-	function test_multiple_visits() {
-		$this->load_bookmark_add_page();
-
-		$input_title = String::uuid();
-		$input_url = "http://127.0.0.1/";
-		$this->assertTrue($this->setField('data[Bookmark][title]',
-			$input_title));
-		$this->assertTrue($this->setField('data[Bookmark][url]', $input_url));
-		$this->click("Submit");
-
-		$this->verify_page_load();
-
-		$current_url = $this->getUrl();
-
-
-		for ($i = 0; $i < 10; $i++) {
-			$this->click($input_url);
-			$this->get($current_url);
-			$this->verify_page_load();
-		}
-		$this->assertPattern("/Visits: $i\D+/");
-
-		$this->get($this->baseurl.'/');
-		$this->verify_page_load();
-		$this->assertPattern('/most visits.*'.$input_title
-			.'.*recently visited/');
-	}
 }
 ?>
