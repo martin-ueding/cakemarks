908a19d8f6a8b96813ca2812b6d1bf0c1ddd69db
Martin Ueding <dev@martin-ueding.de>
2011-08-30 19:43:19 +0200

    trying constructor

--- models/bookmark.php
+++ models/bookmark.php
@@ -4,38 +4,6 @@
 class Bookmark extends AppModel {
 	var $name = 'Bookmark';
 	var $displayField = 'title';
-	var $validate = array(
-		'url' => array(
-			'url' => array(
-				'rule' => array('url'),
-				'message' => __('Enter a valid URL', true),
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-		'revisit' => array(
-			'numeric' => array(
-				'rule' => array('numeric'),
-				//'message' => 'Your custom message here',
-				'allowEmpty' => true,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-		'reading_list' => array(
-			'boolean' => array(
-				'rule' => array('boolean'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-	);
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
 
 	var $hasMany = array(
@@ -73,5 +41,39 @@ class Bookmark extends AppModel {
 		)
 	);
 
+	function __construct() {
+		$this->validate = array(
+			'url' => array(
+				'url' => array(
+					'rule' => array('url'),
+					'message' => __('Enter a valid URL', true),
+					//'allowEmpty' => false,
+					//'required' => false,
+					//'last' => false, // Stop validation after this rule
+					//'on' => 'create', // Limit validation to 'create' or 'update' operations
+				),
+			),
+			'revisit' => array(
+				'numeric' => array(
+					'rule' => array('numeric'),
+					//'message' => 'Your custom message here',
+					'allowEmpty' => true,
+					//'required' => false,
+					//'last' => false, // Stop validation after this rule
+					//'on' => 'create', // Limit validation to 'create' or 'update' operations
+				),
+			),
+			'reading_list' => array(
+				'boolean' => array(
+					'rule' => array('boolean'),
+					//'message' => 'Your custom message here',
+					//'allowEmpty' => false,
+					//'required' => false,
+					//'last' => false, // Stop validation after this rule
+					//'on' => 'create', // Limit validation to 'create' or 'update' operations
+				),
+			),
+		);
+	}
 }
 ?>
