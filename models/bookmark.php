bf45c7b6dd4a730d65de00de8320f80b40bea9c2
Martin Ueding <dev@martin-ueding.de>
2011-08-31 12:00:12 +0200

    removed unneeded comment

--- models/bookmark.php
+++ models/bookmark.php
@@ -37,7 +37,6 @@ class Bookmark extends AppModel {
 			),
 		),
 	);
-	//The Associations below have been created with all possible keys, those that are not needed can be removed
 
 	var $hasMany = array(
 		'Visit' => array(
