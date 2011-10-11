e7fdcb3f799456417985c104806abc75a656c826
Martin Ueding <dev@martin-ueding.de>
2011-10-11 20:20:55 +0200

    Use cacheing method, server side pickup

--- views/helpers/favico.php
+++ views/helpers/favico.php
@@ -2,11 +2,33 @@
 # Copyright (c) 2011 Martin Ueding <dev@martin-ueding.de>
 
 class FavicoHelper extends AppHelper {
+	var $tries = 3;
+
 	function get($url) {
+		if ($this->tries-- < 0)
+			return;
+
 		$url = trim(str_replace('http://', '', trim($url)), '/');
         $url = explode('/', $url);
-        $url = 'http://' . $url[0] . '/';
+		$hash = md5($url[0]);
+        $url = 'http://' . $url[0] . '/favicon.ico';
 
-		return $url.'favico.ico';
+		$dir = 'cache/favico';
+		$file = $dir.'/'.$hash;
+
+		if (!file_exists($dir)) {
+			if (!mkdir($dir, 0777, true)) {
+				die(__('Could not create favico temp dir', true));
+			}
+		}
+
+		if (!file_exists($file)) {
+			$h = fopen($file, "w");
+			fwrite($h, file_get_contents($url));
+			fclose($h);
+		}
+
+		return $file;
+
 	}
 }
