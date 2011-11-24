9b45e3a3723623e50129e29c70fa452ea1145b0a
Martin Ueding <dev@martin-ueding.de>
2011-11-24 16:17:22 +0100

    Set var to false as default

--- controllers/bookmarks_controller.php
+++ controllers/bookmarks_controller.php
@@ -251,6 +251,9 @@ class BookmarksController extends AppController {
 			$this->_import(json_decode($this->data['Bookmark']['json'], true));
 			$this->set('show_results', true);
 		}
+		else {
+			$this->set('show_results', false);
+		}
 
 		$this->set('show_form', true);
 		$this->set('import_result', $this->import_result);
