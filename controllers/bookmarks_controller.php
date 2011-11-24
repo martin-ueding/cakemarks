074e6b4a83fe1882795843a886b4494d4915a4c6
Martin Ueding <dev@martin-ueding.de>
2011-11-24 16:14:42 +0100

    Only add keywords if they are set

--- controllers/bookmarks_controller.php
+++ controllers/bookmarks_controller.php
@@ -272,20 +272,24 @@ class BookmarksController extends AppController {
 			# Build a CakePHP style array.
 			$q['Bookmark']['title'] = $bookmark['title'];
 			$q['Bookmark']['url'] = $bookmark['url'];
-			foreach ($bookmark['keywords'] as $keyword) {
-				$db_keyword = $this->Keyword->find('first', array(
-					'conditions' => array(
-						'Keyword.title' => $keyword)
-					));
 
-				if (isset($db_keyword['Keyword']['id'])) {
-					$q['Keyword'][] = $db_keyword['Keyword']['id'];
-					$this->import_result['existing_keywords']++;
-				}
-				else {
-					$this->Keyword->save(array('title' => $keyword));
-					$q['Keyword'][] = $this->Keyword->id;
-					$this->import_result['added_keywords']++;
+			# If there are any keywords, add them to the array.
+			if (isset($bookmark['keywords'])) {
+				foreach ($bookmark['keywords'] as $keyword) {
+					$db_keyword = $this->Keyword->find('first', array(
+						'conditions' => array(
+							'Keyword.title' => $keyword)
+						));
+
+					if (isset($db_keyword['Keyword']['id'])) {
+						$q['Keyword'][] = $db_keyword['Keyword']['id'];
+						$this->import_result['existing_keywords']++;
+					}
+					else {
+						$this->Keyword->save(array('title' => $keyword));
+						$q['Keyword'][] = $this->Keyword->id;
+						$this->import_result['added_keywords']++;
+					}
 				}
 			}
 
@@ -300,12 +304,12 @@ class BookmarksController extends AppController {
 			# Go to the next bookmark if this already exists.
 			if ($count > 0) {
 				$this->import_result['existing_bookmarks']++;
-				continue;
 			}
-
 			# Add bookmark to the database.
-			$this->Bookmark->save($q);
-			$this->import_result['added_bookmarks']++;
+			else {
+				$this->Bookmark->save($q);
+				$this->import_result['added_bookmarks']++;
+			}
 
 			unset($q);
 		}
