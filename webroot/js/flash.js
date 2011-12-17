dddfa5357e622e9ea70f3fcf48a163f7f915caba
Martin Ueding <dev@martin-ueding.de>
2011-12-17 15:22:30 +0100

    Delete old JS files

--- webroot/js/flash.js
+++
@@ -1,5 +0,0 @@
-// Copyright (c) 2011 Martin Ueding <dev@martin-ueding.de>
-
-$(document).ready(function() {
-	$("#flashMessage").delay(3000).animate({opacity: 0.5}, 400).delay(2000).slideUp(400);
-});
