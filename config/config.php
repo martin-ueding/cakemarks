440582f8f182cf8118f1a5d529b7c62be515a53d
Martin Ueding <dev@martin-ueding.de>
2011-12-18 13:52:59 +0100

    Use configure

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+# Copyright (c) 2011 Martin Ueding <dev@martin-ueding.de>
+
+$config["UI"] = array(
+	"Startscreen" => array(
+		"BoxLength" => 30
+	),
+	"VisitGraph" => array(
+		"BinCount" => 12
+	)
+);
