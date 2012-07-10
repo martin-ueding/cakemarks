10d89e5489dbea118549847769ed6bc2a5e62c34
Martin Ueding <dev@martin-ueding.de>
2012-07-10 08:53:34 +0200

    Sort search entries by visit count

--- controllers/bookmarks_controller.php
+++ controllers/bookmarks_controller.php
@@ -348,8 +348,17 @@ class BookmarksController extends AppController {
 			$conditions[] = array('Bookmark.title LIKE' => '%'.$word.'%');
 		}
 		$data = $this->Bookmark->find('all', array(
+			'fields' => array('Bookmark.id', 'Bookmark.title', 'Bookmark.url', 'count(Bookmark.id)'),
 			'conditions' => $conditions,
 			'limit' => Configure::read('search.items'),
+			'group' => 'cakemarks_visits.bookmark_id',
+			'joins' => array(
+				array(
+					'table' => 'cakemarks_visits',
+					'conditions' => array('cakemarks_visits.bookmark_id = Bookmark.id')
+				)
+			),
+			'order' => 'count(Bookmark.id) DESC',
 		));
 
 		$this->set("data", json_encode($data));
