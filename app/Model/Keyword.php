241385311eef351adb7fe7d70741087bffcae212
Martin Ueding <dev@martin-ueding.de>
2013-09-30 12:34:55 +0200

    Retab keyword

--- app/Model/Keyword.php
+++ app/Model/Keyword.php
@@ -1,82 +1,82 @@
 <?php
-# Copyright © 2012 Martin Ueding <dev@martin-ueding.de>
+# Copyright © 2012-2013 Martin Ueding <dev@martin-ueding.de>
 # Licensed under the MIT License (http://www.opensource.org/licenses/mit-license.php).
 
 class Keyword extends AppModel {
-	var $name = 'Keyword';
-	var $displayField = 'title';
-	var $validate = array(
-		'title' => array(
-			'notempty' => array(
-				'rule' => array('notempty'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-			'isUnique' => array(
-				'rule' => 'isUnique',
-				'message' => 'The URL has to be unique.',
-			),
-		),
-		'sticky' => array(
-			'boolean' => array(
-				'rule' => array('boolean'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-	);
-	//The Associations below have been created with all possible keys, those that are not needed can be removed
+    var $name = 'Keyword';
+    var $displayField = 'title';
+    var $validate = array(
+        'title' => array(
+            'notempty' => array(
+                'rule' => array('notempty'),
+                //'message' => 'Your custom message here',
+                //'allowEmpty' => false,
+                //'required' => false,
+                //'last' => false, // Stop validation after this rule
+                //'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+            'isUnique' => array(
+                'rule' => 'isUnique',
+                'message' => 'The URL has to be unique.',
+            ),
+        ),
+        'sticky' => array(
+            'boolean' => array(
+                'rule' => array('boolean'),
+                //'message' => 'Your custom message here',
+                //'allowEmpty' => false,
+                //'required' => false,
+                //'last' => false, // Stop validation after this rule
+                //'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+        ),
+    );
+    //The Associations below have been created with all possible keys, those that are not needed can be removed
 
-	var $belongsTo = array(
-		'ParentKeyword' => array(
-			'className' => 'Keyword',
-			'foreignKey' => 'parent_id',
-			'conditions' => '',
-			'fields' => '',
-			'order' => ''
-		)
-	);
+    var $belongsTo = array(
+        'ParentKeyword' => array(
+            'className' => 'Keyword',
+            'foreignKey' => 'parent_id',
+            'conditions' => '',
+            'fields' => '',
+            'order' => ''
+        )
+    );
 
-	var $hasMany = array(
-		'ChildKeyword' => array(
-			'className' => 'Keyword',
-			'foreignKey' => 'parent_id',
-			'dependent' => false,
-			'conditions' => '',
-			'fields' => '',
-			'order' => '',
-			'limit' => '',
-			'offset' => '',
-			'exclusive' => '',
-			'finderQuery' => '',
-			'counterQuery' => ''
-		)
-	);
+    var $hasMany = array(
+        'ChildKeyword' => array(
+            'className' => 'Keyword',
+            'foreignKey' => 'parent_id',
+            'dependent' => false,
+            'conditions' => '',
+            'fields' => '',
+            'order' => '',
+            'limit' => '',
+            'offset' => '',
+            'exclusive' => '',
+            'finderQuery' => '',
+            'counterQuery' => ''
+        )
+    );
 
 
-	var $hasAndBelongsToMany = array(
-		'Bookmark' => array(
-			'className' => 'Bookmark',
-			'joinTable' => 'bookmarks_keywords',
-			'foreignKey' => 'keyword_id',
-			'associationForeignKey' => 'bookmark_id',
-			'unique' => true,
-			'conditions' => '',
-			'fields' => '',
-			'order' => '',
-			'limit' => '',
-			'offset' => '',
-			'finderQuery' => '',
-			'deleteQuery' => '',
-			'insertQuery' => ''
-		)
-	);
+    var $hasAndBelongsToMany = array(
+        'Bookmark' => array(
+            'className' => 'Bookmark',
+            'joinTable' => 'bookmarks_keywords',
+            'foreignKey' => 'keyword_id',
+            'associationForeignKey' => 'bookmark_id',
+            'unique' => true,
+            'conditions' => '',
+            'fields' => '',
+            'order' => '',
+            'limit' => '',
+            'offset' => '',
+            'finderQuery' => '',
+            'deleteQuery' => '',
+            'insertQuery' => ''
+        )
+    );
 
 }
 ?>
